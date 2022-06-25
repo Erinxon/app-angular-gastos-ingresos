99c79da4ed64bb682ca8c8f36d759c2794d48e17
Erinxon <30541728+Erinxon@users.noreply.github.com>
2022-06-25 19:41:24 -0400

    change

--- Proyecto_final.Server/Tools/GenerateJWT.cs
+++ Proyecto_final.Server/Tools/GenerateJWT.cs
@@ -20,14 +20,14 @@ namespace Proyecto_final.Server.Tools
 
             var claims = new[]
             {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.GivenName, user.FirstName),
-                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim("id", user.Id.ToString()),
+                new Claim("email", user.Email),
+                new Claim("firstName", user.FirstName),
+                new Claim("lastName", user.LastName),
             };
 
             var token = new JwtSecurityToken(config["Jwt:Issuer"], config["Jwt:Audience"],
-              claims, expires: DateTime.Now.AddDays(2),
+              claims, expires: DateTime.Now.AddDays(1),
               signingCredentials: credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
